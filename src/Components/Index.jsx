3854e061f4c7d50e019fcfd257d2cb5288cdf6ba
Jesse <jessemckinney513@gmail.com>
2021-04-07 11:03:34 -0400

    yay

---
+++ src/Components/Index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import styles from "./Index.module.css";
+
+class Index extends Component {
+  render() {
+    return (
+      <>
+        <div className={styles.wrapper}>
+          <header className={styles.header}></header>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Index;
