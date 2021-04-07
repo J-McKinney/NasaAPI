4624432ad604e5bf7d007927843b1f8db381245d
Jesse <jessemckinney513@gmail.com>
2021-04-07 11:19:07 -0400

    yay

--- src/Components/Index.jsx
+++ src/Components/Index.jsx
@@ -1,7 +1,28 @@
 import React, { Component } from "react";
 import styles from "./Index.module.css";
+import axios from "axios";
+require("dotenv").config();
 
 class Index extends Component {
+  state = {
+    nasaInfo: [],
+  };
+
+  componentDidMount() {
+    const nasaAPI =
+      "https://api.nasa.gov/planetary/apod?api_key=" +
+      process.env.REACT_APP_APIKey;
+    axios
+      .get(nasaAPI)
+      .then((res) => {
+        console.log(res);
+        return axios.get(nasaAPI);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <>
